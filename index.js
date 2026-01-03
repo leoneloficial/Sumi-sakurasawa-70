580f4ab33e9ea071be8225837de8c8f294892cc9
Leonel <166152300+leoneloficial@users.noreply.github.com>
2026-01-03 01:35:57 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './settings.js'
 import './plugins/_allfake.js'
@@ -134,8 +135,7 @@ msgRetryCounterCache: msgRetryCounterCache || new Map(),
 userDevicesCache: userDevicesCache || new Map(),
 defaultQueryTimeoutMs: undefined,
 cachedGroupMetadata: (jid) => global.conn.chats[jid] ?? {},
-version: [2, 3000, 1027934701],
-//version: version,
+version: version,
 keepAliveIntervalMs: 55000,
 maxIdleTimeMs: 60000,
 }
