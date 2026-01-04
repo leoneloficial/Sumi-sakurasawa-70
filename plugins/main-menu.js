9d5fe4f06b2d3bcfa97074acd37d35a63d092ce4
Leonel <166152300+leoneloficial@users.noreply.github.com>
2026-01-04 14:35:56 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -466,7 +466,7 @@ body: textbot,
 mediaType: 1,
 mediaUrl: redes,
 sourceUrl: redes,
-thumbnail: await (await fetch(conn.botBanner)).buffer(),
+thumbnail: await (await fetch(conn.botbanner)).buffer(),
 showAdAttribution: false,
 containsAutoReply: true,
 renderLargerThumbnail: true
