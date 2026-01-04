181c27e752c59a96ada8c7d9296619919b9e3f21
Leonel <166152300+leoneloficial@users.noreply.github.com>
2026-01-04 14:42:02 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -8,7 +8,7 @@ let userId = mentionedJid && mentionedJid[0] ? mentionedJid[0] : m.sender
 let totalreg = Object.keys(global.db.data.users).length
 let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
 
-let txt = ` > ❀ hola! ${m.pushName}, Soy ${botname}, Aquí tienes la lista de comandos.
+let txt = ` > ❀ hola! ${m.pushName}, Soy ${conn.botName}, Aquí tienes la lista de comandos.
 
 *❀ canal oficial »* https://whatsapp.com/channel/0029Vagdmfv1SWt5nfdR4z3w
 
@@ -466,7 +466,7 @@ body: textbot,
 mediaType: 1,
 mediaUrl: redes,
 sourceUrl: redes,
-thumbnail: await (await fetch(banner)).buffer(),
+thumbnail: await (await fetch(conn.Banner)).buffer(),
 showAdAttribution: false,
 containsAutoReply: true,
 renderLargerThumbnail: true
