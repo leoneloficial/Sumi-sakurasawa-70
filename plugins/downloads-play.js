98fe867a40366fa11f7852cc2733dbff0396ca73
Leonel <166152300+leoneloficial@users.noreply.github.com>
2026-01-28 06:00:43 -0400

    Actualizar downloads-play.js

--- plugins/downloads-play.js
+++ plugins/downloads-play.js
@@ -45,7 +45,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
     }
 }
 
-handler.command = /^(play2|mp4|
+handler.command = /^(play|yta|ytmp3|play2|ytv|ytmp4|playaudio|mp4)$/i
 handler.group = false
 
 export default handler
